e5e822639d1045cf9b9947c14282295d51d03a53
thr3a <12jk172@ms.dendai.ac.jp>
2019-04-13 10:35:34 +0900

    add: クリップボードにコピーと再生成ボタン実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,42 @@
 <template>
-  <section class="container">
+  <section class="container mt-4">
     <div class="jumbotron">
       <h1 class="display-3">ua-factory</h1>
-      <p class="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
+      <p class="lead">ユーザーエージェントをランダムに生成します。</p>
     </div>
-    <div>
-      <input type="text" v-model="ua" class="ua-text">
-      <button v-on:click="copy">クリップボードにコピー</button>
-    </div>
+    <input type="text" v-model="ua" id="ua-text" class="form-control mb-2">
+    <button class="btn btn-primary" @click="reload">再生成</button>
+    <button class="btn-copy btn btn-success" data-clipboard-target="#ua-text">クリップボードにコピー</button>
   </section>
 </template>
 
 <script>
 import UserAgent from "user-agents";
+import clipboard from "clipboard";
 
 export default {
   asyncData(context) {
     const userAgent = new UserAgent();
-    return { ua: userAgent.toString() };
+    return {
+      ua: userAgent.toString()
+    };
   },
   methods: {
-    copy: function() {
-      console.log(1);
+    reload: function() {
+      const ua = new UserAgent().toString();
+      this.$data.ua = ua;
     }
+  },
+  mounted: function() {
+    this.clipBoard = new clipboard(".btn-copy");
+    this.clipBoard.on("success", function(e) {
+      console.log("copied!");
+      e.clearSelection();
+    });
+    this.clipBoard.on("error", function(e) {});
   }
 };
 </script>
 
 <style>
-.ua-text {
-  width: 100%;
-}
 </style>
