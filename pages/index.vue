2d1b1f1e498c67dbc0c5f2dbfdf23f2fa6db926e
thr3a <12jk172@ms.dendai.ac.jp>
2019-04-13 14:55:45 +0900

    update: デバイスは１種類しか選べないようにした

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,25 @@
       <h1 class="display-3">ua-factory</h1>
       <p class="lead">ユーザーエージェントをランダムに生成します。</p>
     </div>
-    <form>
-      <div class="form-group">
-        <p>デバイス</p>
-        <input type="checkbox" id="pc" value="desktop" v-model="devices" v-on:change="reload">
-        <label for="pc">PC</label>
-        <input type="checkbox" id="mobile" value="mobile" v-model="devices" v-on:change="reload">
-        <label for="mobile">スマートフォン</label>
-        <input type="checkbox" id="tablet" value="tablet" v-model="devices" v-on:change="reload">
-        <label for="tablet">タブレット</label>
-      </div>
-      <div class="form-group">
-        <input type="text" :value="ua_single" id="ua-text" class="form-control mb-2">
-        <button class="btn btn-primary" @click="reload">再生成</button>
-        <button class="btn-copy btn btn-success" data-clipboard-target="#ua-text">クリップボードにコピー</button>
-      </div>
-      <div class="form-group">
-        <textarea id="ua-texts" rows="10" :value="ua_multiple"></textarea>
-      </div>
-    </form>
+    <div class="form-group">
+      <p>デバイス</p>
+      <input type="radio" id="all" value="all" v-model="device" v-on:change="reload">
+      <label for="all">全て</label>
+      <input type="radio" id="pc" value="desktop" v-model="device" v-on:change="reload">
+      <label for="pc">PC</label>
+      <input type="radio" id="mobile" value="mobile" v-model="device" v-on:change="reload">
+      <label for="mobile">スマートフォン</label>
+      <input type="radio" id="tablet" value="tablet" v-model="device" v-on:change="reload">
+      <label for="tablet">タブレット</label>
+    </div>
+    <div class="form-group">
+      <input type="text" :value="ua_single" id="ua-text" class="form-control mb-2">
+      <button class="btn btn-primary" @click="reload">再生成</button>
+      <button class="btn-copy btn btn-success" data-clipboard-target="#ua-text">クリップボードにコピー</button>
+    </div>
+    <div class="form-group">
+      <textarea id="ua-texts" rows="10" :value="ua_multiple"></textarea>
+    </div>
   </section>
 </template>
 
@@ -33,7 +33,7 @@ import clipboard from "clipboard";
 export default {
   data() {
     return {
-      devices: ["desktop", "mobile", "tablet"]
+      device: "all"
     };
   },
   async asyncData(context) {
@@ -41,15 +41,27 @@ export default {
     return {
       ua: ua,
       ua_single: ua.random().userAgent,
-      ua_multiple: Array.from({ length: 10 }, (v, k) => ua.random().userAgent).join("\n")
+      ua_multiple: Array.from(Array(10), (v, k) => ua.random().userAgent).join("\n")
     };
   },
   methods: {
     reload: function() {
-      this.$data.ua = new UserAgent(data => {
-        return this.$data.devices.indexOf(data.deviceCategory) >= 0;
-      });
+      const filter = {};
+      if (this.$data.device !== "all") {
+        filter.deviceCategory = this.$data.device;
+      }
+      // const filter = [
+      //   /Safari/,
+      //   {
+      //     platform: "MacIntel"
+      //   }
+      // ];
+      this.$data.ua = new UserAgent(filter);
+      // this.$data.ua = new UserAgent(data => {
+      //   return this.$data.device.indexOf(data.deviceCategory) >= 0;
+      // });
       this.$data.ua_single = this.$data.ua.random().userAgent;
+      this.$data.ua_multiple = Array.from(Array(10), (v, k) => this.$data.ua.random().userAgent).join("\n")
     }
   },
   mounted: function() {
