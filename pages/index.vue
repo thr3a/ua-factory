5ae01aca5c91b7b66755ed7e4bfc22223dfc23c8
thr3a <12jk172@ms.dendai.ac.jp>
2019-04-13 09:59:34 +0900

    add: UAランダム表示実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,37 @@
 <template>
   <section class="container">
+    <div class="jumbotron">
+      <h1 class="display-3">ua-factory</h1>
+      <p class="lead">
+        This is a simple hero unit, a simple jumbotron-style component for
+        calling extra attention to featured content or information.
+      </p>
+    </div>
     <div>
-      <logo />
-      <h1 class="title">
-        ua-factory
-      </h1>
-      <h2 class="subtitle">
-        My kickass Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
+      <input type="text" v-model="ua" class="ua-text">
+      <button v-on:click="copy">クリップボードにコピー</button>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import UserAgent from "user-agents";
 
 export default {
-  components: {
-    Logo
+  asyncData(context) {
+    const userAgent = new UserAgent();
+    return { ua: userAgent.toString() };
+  },
+  methods: {
+    copy: function() {
+      console.log(1);
+    }
   }
-}
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.ua-text {
+  width: 100%;
 }
 </style>
